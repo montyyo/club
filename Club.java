84e371a3b9b4a594d96f008e8b5c5b557639ed09
montyyo <marcoiban@gmail.com>
2015-01-30 14:23:05 +0100

    metodos para mostrar socios registerados cada mes y metodo para ver lso socios dados de baja

--- Club.java
+++ Club.java
@@ -36,4 +36,67 @@ public class Club
     {
         return socio.size();
     }
+    
+    /** 
+   * Calcula el numero de socios que se dieron de alta en un mes determinado. 
+     * El año no nos importa. En caso de que el parametro contenga un valor 
+       * no valido se muestra por pantalla el error.
+         * @param month El mes en el que estamos interesados
+           * @return El numero de socios que se dieron de alta dicho mes
+             */
+     public int joinedMonth(int month)
+     {
+         int sociosMes = 0;
+         
+         if( month < 1 ||  month > 12)
+         {
+             System.out.println(" ERROR --> introduce un numero entre 1 y 12 ambos incluidos");
+         }
+         else
+         {
+             for(Membership socios : socio)
+             {
+                 if(socios.getMonth() == month)
+                 {
+                     sociosMes++;
+                 }
+             }
+         }
+         return sociosMes;
+     }
+     
+     
+     /** 
+   * Todos los socios que se han dado de alta un determinado mes de un determinado año se
+   * dan de baja. En caso de que el parametro month contenga un valor no valido se muestra 
+   * por pantalla el error.
+   * @param month El mes en el que estamos interesados
+   * @param year El año en el que estamos interesados
+   * @return Una coleccion con los socios que se han dado de baja del club
+   */
+  
+  public ArrayList<Membership> purge(int month , int year)
+  {
+      ArrayList<Membership> bajas = new ArrayList<Membership>();
+      
+     
+         
+         if( month < 1 ||  month > 12)
+         {
+             System.out.println(" ERROR --> introduce un numero entre 1 y 12 ambos incluidos");
+         }
+         else
+         {
+             for(Membership socios : socio)
+             {
+                 if(socios.getMonth() == month && socios.getYear() == year )
+                 {
+                     bajas.add(socios);
+                     socio.remove(socios);
+                     
+                 }
+             }
+         }
+     return bajas;
+  }
 }
---
+++ Test.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList; 
+/**
+ * Write a description of class Test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    //public ArrayList<Membership> test()
+    //{
+    //   ArrayList<Membership> prueba = new ArrayList<Membership>();
+       
+     //  Membership membersh1 = new Membership( "pepe" , 1 ,123);
+       
+    //   Membership membersh2 = new Membership("juan" , 2, 123);
+       
+      
+     //  Club club1 = new Club();
+       
+       //club1.add(membersh1);
+       //club1.add(membersh2);
+       
+       //return prueba.purge();
+    //}
+    
+}
